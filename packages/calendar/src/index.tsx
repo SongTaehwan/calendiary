0e162b0e7ed869c25eb8dbfdac6f23857fc18b93
송태환 <axoghks@gmail.com>
2025-10-16 22:26:52 +0900

    build(calendar): calendar 라이브러리 초기화

---
+++ packages/calendar/src/index.tsx
@@ -0,0 +1,8 @@
+export function multiply(a: number, b: number): number {
+  console.log('multiply22', a, b);
+  return a * b;
+}
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
