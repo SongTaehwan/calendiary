5caba8ba9a3d879d9f321783d5cd76cf2eec7a28
송태환 <axoghks@gmail.com>
2025-10-19 01:34:47 +0900

    perf(calendar): 높이 전환 애니메이션 성능 개선
    - react-native-reanimated 로 구현체 변경

---
+++ packages/calendar/src/hooks/utils/useAnimatedHeightTransition.ts
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { useSharedValue, withTiming } from 'react-native-reanimated';
+
+interface UseAnimatedHeightTransitionProps {
+  height: number;
+  duration: number;
+}
+
+const useAnimatedHeightTransition = ({
+  height,
+  duration,
+}: UseAnimatedHeightTransitionProps) => {
+  const animatedHeight = useSharedValue(height);
+
+  useEffect(() => {
+    animatedHeight.value = withTiming(height, {
+      duration,
+    });
+  }, [height, duration, animatedHeight]);
+
+  return animatedHeight;
+};
+
+export default useAnimatedHeightTransition;
