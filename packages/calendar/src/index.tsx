5688ec9ca6d4ed222a48791fd86813c0cd15a957
송태환 <axoghks@gmail.com>
2025-10-19 19:24:26 +0900

    feat(calendar): 라이브러리 헤더 파일 업데이트

--- packages/calendar/src/index.tsx
+++ packages/calendar/src/index.tsx
@@ -1,9 +1,11 @@
 // calendars
 export { default as Calendar } from './calendars/Calendar';
+export { default as ExpandableCalendar } from './calendars/ExpandableCalendar';
 
 // utils
 export { LocaleConfig } from './utils/locale';
 
 // types
 export type { CalendarProps } from './calendars/Calendar';
+export type { ExpandableCalendarProps } from './calendars/ExpandableCalendar';
 export type { LocaleKey, SupportedLanguage } from './utils/locale';
